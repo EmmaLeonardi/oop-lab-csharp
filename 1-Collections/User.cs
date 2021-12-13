87644ed608aa1545df2315b7397098857e3ea2f3
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2021-12-13 11:12:40 +0100

    Implemented Users

--- 1-Collections/User.cs
+++ 1-Collections/User.cs
@@ -6,8 +6,17 @@ namespace Collections
     {
         public User(string fullName, string username, uint? age)
         {
-            throw new NotImplementedException("TODO ensure that username is not null");
-            throw new NotImplementedException("TODO initialise this instance of user accordingly");
+            if (username != null)
+            {
+                Username = username;
+            }
+            else
+            {
+                throw new ArgumentNullException("Username cannot be null");
+            }
+
+            Age = age;
+            FullName = fullName;
         }
         
         public uint? Age { get; }
@@ -16,8 +25,7 @@ namespace Collections
         
         public string Username { get; }
 
-        public bool IsAgeDefined => throw new NotImplementedException("TODO check whether age is non-null or not");
+        public bool IsAgeDefined => Age != null;
         
-        // TODO implement missing methods (try to autonomously figure out which are the necessary methods)
     }
 }
